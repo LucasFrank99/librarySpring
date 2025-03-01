8c5f3b7edf1280a5afb70a7de6f2826c7250355f
lucas frank <lucasfrank11@hotmail.com>
2025-03-01 01:44:01 -0300

    Implemented methods getAllBooks (GET), createBook (POST) and deleteBook (DELETE) in the BookService class

--- src/main/java/com/lfrank/libraryProject/controller/BookController.java
+++ src/main/java/com/lfrank/libraryProject/controller/BookController.java
@@ -1,7 +1,13 @@
 package com.lfrank.libraryProject.controller;
 
+import com.lfrank.libraryProject.models.Book;
 import com.lfrank.libraryProject.service.BookService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/library")
 public class BookController {
 
     private final BookService bookService;
@@ -9,4 +15,24 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
+
+    //List all books
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.bookList();
+    }
+
+    //Create a new book
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.save(book);
+    }
+
+    //Delete a book
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.delete(id);
+    }
+
+
 }
